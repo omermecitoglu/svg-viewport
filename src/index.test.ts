766467bae43e9f319f49b84279d2547d92a3f906
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-10-02 08:53:35 +0700

    chore: add jest

---
+++ src/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "@jest/globals";
+import SvgViewport from "~/components/SvgViewport";
+
+describe("SvgViewport Component", () => {
+  it("should ...", () => {
+    expect(SvgViewport).not.toBeNull();
+  });
+});
