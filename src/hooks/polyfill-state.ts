fd2e75464aaa9fcec8d3bbfdc0ba1eda6d4b8320
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-10-05 07:24:53 +0700

    chore: update eslint

--- src/hooks/polyfill-state.ts
+++ src/hooks/polyfill-state.ts
@@ -2,7 +2,7 @@ import { type Dispatch, useState } from "react";
 
 export function usePolyfillState<T>(
   state: T,
-  dispatch?: Dispatch<React.SetStateAction<T>>
+  dispatch?: Dispatch<React.SetStateAction<T>>,
 ): [T, Dispatch<React.SetStateAction<T>>] {
   const [polyfill, setPolyfill] = useState<T>(state);
   if (dispatch) {
